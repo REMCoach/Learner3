43d8e6753d9bc1434cbdb65658dc437478e6f22a
Sooriyakum <74105617+Sooriyakum@users.noreply.github.com>
2020-12-02 00:16:19 -0800

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,1 @@
-  
-const { Router } = require('express');
 
-const cmscontent = require('./api/cmscontent/cms.routes');
-
-const router = Router();
-
-router.get('/', (req, res) => {
-  res.statusCode = 302;
-  res.setHeader('Location', 'https://difuza.com/');
-  res.end();
-});
-
-router.use('/cmscontent', cmscontent);
-
-module.exports = router;
